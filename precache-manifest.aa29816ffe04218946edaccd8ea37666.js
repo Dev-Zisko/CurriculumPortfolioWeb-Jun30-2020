40156b5a6b5f98880e7347b063addbff34ab6741
Dev-Zisko <dev.oliverfrancisco@gmail.com>
2020-06-30 23:17:15 -0400

    Updates

---
+++ precache-manifest.aa29816ffe04218946edaccd8ea37666.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956a0ea24e2bd351c8723c1028d29883",
+    "url": "/curriculum-portfolio/index.html"
+  },
+  {
+    "revision": "b9c4222519a2ac5cf5e0",
+    "url": "/curriculum-portfolio/static/css/main.dcd419e5.chunk.css"
+  },
+  {
+    "revision": "17f5a71ece4447319b7e",
+    "url": "/curriculum-portfolio/static/js/2.7834407a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curriculum-portfolio/static/js/2.7834407a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c4222519a2ac5cf5e0",
+    "url": "/curriculum-portfolio/static/js/main.1719416b.chunk.js"
+  },
+  {
+    "revision": "91068f97957b77861f41",
+    "url": "/curriculum-portfolio/static/js/runtime-main.133326c8.js"
+  },
+  {
+    "revision": "3767092a51c3bcb75e8d47d209e3e93e",
+    "url": "/curriculum-portfolio/static/media/about.3767092a.jpg"
+  },
+  {
+    "revision": "373191be75c8dd6a5746cb9d49d0c391",
+    "url": "/curriculum-portfolio/static/media/partner-1.373191be.png"
+  },
+  {
+    "revision": "ff9372cc9f94c23ab6533ce3eb910ea4",
+    "url": "/curriculum-portfolio/static/media/partner-2.ff9372cc.png"
+  },
+  {
+    "revision": "b1d8da478569b4f45197bb208c8e7475",
+    "url": "/curriculum-portfolio/static/media/partner-3.b1d8da47.png"
+  },
+  {
+    "revision": "66638d1eda0eb21c00a0cb8167d5bbba",
+    "url": "/curriculum-portfolio/static/media/partner-4.66638d1e.png"
+  },
+  {
+    "revision": "63f34cce722e8ef8090bee5deb32f125",
+    "url": "/curriculum-portfolio/static/media/partner-5.63f34cce.png"
+  },
+  {
+    "revision": "8d43c4f6460167bf28fa4c34b4283bd0",
+    "url": "/curriculum-portfolio/static/media/work-1.8d43c4f6.jpg"
+  },
+  {
+    "revision": "66473aca3e7db679bccc668862a95be0",
+    "url": "/curriculum-portfolio/static/media/work-2.66473aca.jpg"
+  },
+  {
+    "revision": "76a6b8fd06ff43c7d5d3d25d0682bb07",
+    "url": "/curriculum-portfolio/static/media/work-3.76a6b8fd.jpg"
+  },
+  {
+    "revision": "d9e65ec7bad563eb40e96864e70b4445",
+    "url": "/curriculum-portfolio/static/media/work-4.d9e65ec7.jpg"
+  },
+  {
+    "revision": "ed4330547b84c9acea3f5e2ef8aa5573",
+    "url": "/curriculum-portfolio/static/media/work-5.ed433054.jpg"
+  },
+  {
+    "revision": "408cbb8ff19ee2b727eb48b6fca93d75",
+    "url": "/curriculum-portfolio/static/media/work-6.408cbb8f.jpg"
+  }
+]);
